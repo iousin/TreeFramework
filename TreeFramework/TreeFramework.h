e9c1244a519b02d1446407f28c083ee61303c393
ahmedmohammed <ahmed.mohammed@underwriteme.co.uk>
2018-11-27 11:02:20 +0000

    Added tree framework project.

---
+++ TreeFramework/TreeFramework.h
@@ -0,0 +1,19 @@
+//
+//  TreeFramework.h
+//  TreeFramework
+//
+//  Created by Ahmed Mohammed on 15/11/2018.
+//  Copyright © 2018 Underwriteme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TreeFramework.
+FOUNDATION_EXPORT double TreeFrameworkVersionNumber;
+
+//! Project version string for TreeFramework.
+FOUNDATION_EXPORT const unsigned char TreeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreeFramework/PublicHeader.h>
+
+
